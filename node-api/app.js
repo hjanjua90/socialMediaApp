f5031e1fe8173bb9934f107c32ac17e969611f8b
hjanjua90 <harpreet_mann23@yahoo.com>
2022-05-25 23:32:41 -0700

    adding frontend and backend

--- node-api/app.js
+++ node-api/app.js
@@ -3,9 +3,12 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
+const fs = require('fs');
+const cors = require('cors')
 const dotenv = require('dotenv');
+
 
 dotenv.config();
 
@@ -22,24 +25,39 @@ mongoose.connection.on('error', err =>{
 //bring in routes
 const postRoutes = require('./routes/post')
 const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+
+//apiDocs
+
+app.get('/', (req,res) =>{
+    fs.readFile('docs/apiDocs.json', (err, data) =>{
+        if(err){
+            res.status(400).json({
+                error: err
+            })
+        }
+        const docs = JSON.parse(data)
+        res.json(docs)
+
+    })
+})
 
 
 //middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
+app.use("/", userRoutes);
 
-// app.use(function (err, req, res, next) {
-//     if (err.name === "UnauthorizedError") {
-//         res.status(401).json({error: "Unauthorized!"});
-//     } 
-//     // else {
-//     //     next(err);
-//     // }
-//     });
+app.use(function (err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).json({error: "Unauthorized!"});
+    } 
+    });
 
 const port = process.env.PORT;
 app.listen(port, ()=>{
