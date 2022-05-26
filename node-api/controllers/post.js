9f756fb3bda304c1f201f89b3fb0ef65c2d016f8
hjanjua90 <harpreet_mann23@yahoo.com>
2022-05-25 23:05:49 -0700

    adding project

---
+++ node-api/controllers/post.js
@@ -0,0 +1,22 @@
+const Post = require('../models/post');
+//let uuidv1 = require('uuidv1')
+
+exports.getPosts = (req, res)=>{
+    const posts = Post.find().select("_id title body")
+    .then((posts)=>{
+        res.json({posts})
+
+    })
+    .catch(err => console.log(err))
+};
+exports.createPost = (req,res)=>{
+    const post = new Post(req.body) 
+    post.save().then(result =>{
+        res.json({
+            post: result
+        });
+
+    });
+
+
+};
---
+++ react-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+class App extends Component{
+  render(){
+    return(
+      <duv>
+        <h1>React Front End</h1>
+        <h2>Adding to react</h2>
+      </duv>
+    );
+  }
+}
+
+export default App;
