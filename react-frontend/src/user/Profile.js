f7b7f5b1a52fcf7a0fff02004a1a528f4e08eb4f
hjanjua90 <harpreet_mann23@yahoo.com>
2022-06-13 10:43:51 -0700

    refractoring profile.js

--- react-frontend/src/user/Profile.js
+++ react-frontend/src/user/Profile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {isAuthenticated} from '../auth';
-
+import {Redirect} from 'react-router-dom';
+import {read} from './apiUser';
 class Profile extends Component {
 
     constructor(){
@@ -10,29 +11,26 @@ class Profile extends Component {
             redirectToSignIn: false
         }
     }
+
+    init = (userId)=>{
+        const token = isAuthenticated().token
+            read(userId, token)
+            .then(data=>{
+                if(data.error){
+                    this.setState({redirectToSignin:true})
+                } else {
+                    this.setState({user: data});
+                }
+            })
+    }
     componentDidMount(){
         const userId = this.props.match.params.userId
-        fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`,{
-            method:"GET",
-            headers:{
-                Accept:"application/json",
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${isAuthenticated().token}`
-            }
-        })
-        .then(response =>{
-            return response.json
-        })
-        .then(data=>{
-            if(data.error){
-                this.setState({redirectToSignin:true})
-            } else {
-                this.setState({user: data});
-            }
-        })
+        this.init(userId);
     }
 
     render(){
+        const redirectToSignIn = this.state.redirectToSignIn
+        if(redirectToSignIn) return<Redirect to="/signin"/>
         return(
             <div className='container'>
                 <h2 className="mt-5 mb-5">Profile</h2>
@@ -42,15 +40,10 @@ class Profile extends Component {
                                 this.state.user.created
                             ).toDateString()}`}
                             </p>
-                {/* <p>{`Joined: ${new Date
-                    (this.state.user.created).toDateString()}`}</p> */}
             </div>
         );
     }
 }
-
-
-
 
 
 
