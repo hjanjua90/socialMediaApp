1bcda94b2a4d32c02b244bf8f800e3713f440632
hjanjua90 <harpreet_mann23@yahoo.com>
2022-06-26 15:00:05 -0700

    adding follow and unfollow to backend

--- node-api/routes/user.js
+++ node-api/routes/user.js
@@ -6,10 +6,18 @@ const {
     updateUser,
     deleteUser,
     userPhoto,
+    addFollowing,
+    addFollower,
+    removeFollowing,
+    removeFollower,
     } = require('../controllers/user');
 const {requireSignin} = require('../controllers/auth');
 
 const router = express.Router();
+
+router.put('/user/follow', requireSignin, addFollowing, addFollower);
+router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower)
+
 
 router.get('/users', allUsers);
 router.get('/user/:userId', requireSignin, getUser);
@@ -21,4 +29,6 @@ router.get("/user/photo/:userId", userPhoto)
 
 //for any route containing :userId, the app will first execute userById()
 router.param("userId", userById)
+
+
 module.exports = router;
