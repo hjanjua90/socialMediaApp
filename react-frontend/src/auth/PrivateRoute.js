a8c4a60b0e97bbbbf59a11c616f2251686eac453
hjanjua90 <harpreet_mann23@yahoo.com>
2022-06-25 20:06:05 -0700

    adding private route

---
+++ react-frontend/src/auth/PrivateRoute.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isAuthenticated } from "./index";
+
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+    // props mean components passed dow tothis private component
+    <Route {...rest} render={props => isAuthenticated()? (
+        <Component{...props}/>
+    ):(
+        <Redirect to={{pathname:"/signin", state:{from: props.location}}}/>
+    )}
+    />
+};
+
+export default PrivateRoute;
